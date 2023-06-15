4cd0b5b0f166fddee2a883515b738ea5353fa800
Guilherme Silva Rodrigues <gui.silva.rodrigues@outlook.com.br>
2023-06-15 17:46:48 -0300

    Att

---
+++ SolicitadorTCC.API/Token/TokenService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using SolicitadorTCC.API.NovaPasta;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SolicitadorTCC.Domain;
+
+namespace SolicitadorTCC.API.Token
+{
+    public static class TokenService
+    {
+        public static string GenerateToken(Pessoa user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Usuario.ToString()),
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(29),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
